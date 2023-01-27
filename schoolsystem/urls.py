418151b1ec6c97e6143c8792ea77a39bdbdcf517
QuameOphory <quameophory@yahoo.com>
2023-01-27 22:52:10 +0000

    Add students url to project url. Update urlpatterns with media url

--- schoolsystem/urls.py
+++ schoolsystem/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from schoolsystem.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('students/', include('students.urls')),
+] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
