d2fd9af46c748bdadda824d517e4b549642ac686
QuameOphory <quameophory@yahoo.com>
2023-01-27 23:09:49 +0000

    Update media files and views

--- students/views.py
+++ students/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import (
+    Student
+)
 # Create your views here.
+
+class StudentListView(generic.ListView):
+    model = Student
+    template_name = "students/students_list.html"
+
