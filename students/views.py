4fcf08cae9f738bb63c47c6a04939e567d5258f2
QuameOphory <quameophory@yahoo.com>
2023-02-12 11:06:51 +0000

    Add get method for student createview

--- students/views.py
+++ students/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.views import generic
+from django.forms import formset_factory
+from .forms import (
+    StudentForm, StudentAlertForm, StudentHobbyForm, StudentGuardianForm,
+    PreviousEducationForm, StudentFavoriteQuoteForm, IDCardForm, 
+    StudentGuardianForm, GuardianForm
+)
 from .models import (
     Student
 )
 # Create your views here.
+
+StudentFormset = formset_factory(StudentForm, extra=1)
+StudentAlertFormset = formset_factory(StudentAlertForm)
+StudentHobbyFormset = formset_factory(StudentHobbyForm)
+StudentGuardianFormset = formset_factory(StudentGuardianForm)
+PreviousEducationFormset = formset_factory(PreviousEducationForm)
+StudentFavoriteQuoteFormset = formset_factory(StudentFavoriteQuoteForm)
+IDCardFormset = formset_factory(IDCardForm)
+GuardianFormset = formset_factory(GuardianForm)
+
+
 
 class StudentListView(generic.ListView):
     model = Student
@@ -14,3 +31,30 @@ class StudentDetailView(generic.DetailView):
     model = Student
     context_object_name = 'student'
     template_name = "students/students_detail.html"
+
+class StudentCreateView(generic.CreateView):
+    model = Student
+    form_class = StudentFormset
+    template_name = "students/add_student.html"
+
+    def get(self, request, *args, **kwargs):
+        # self.object = None
+        student_formset = StudentFormset(prefix = 'students')
+        previous_education_formset = PreviousEducationFormset(prefix='previous_educations')
+        student_alert_formset = StudentAlertFormset(prefix='student_alerts')
+        student_hobby_formset = StudentHobbyFormset(prefix = 'student_hobbies')
+        student_guardian_formset = StudentGuardianFormset(prefix='student_guardians')
+        favorite_quote_formset = StudentFavoriteQuoteFormset(prefix='favorite_quotes')
+        guardian_formset = GuardianFormset(prefix='guardians')
+        return self.render_to_response({
+
+                    'student_formset':student_formset,
+                    'previous_education_formset':previous_education_formset,
+                    'student_alert_formset':student_alert_formset,
+                    'student_hobby_formset':student_hobby_formset,
+                    'student_guardian_formset':student_guardian_formset,
+                    'favorite_quote_formset':favorite_quote_formset,
+                    'guardian_formset': guardian_formset
+            })
+    
+    
